552ceb07a887f66cefaf8ec037d65af338cb8136
sagar <5agarm5hetty05@gmail.com>
2023-01-05 12:22:07 +0530

    first

---
+++ plusminus.js
@@ -0,0 +1,13 @@
+
+var input1 = 0;
+var input2 = 0;
+var input3 = 0;
+
+
+function plus() {
+    var input = document.getElementsByClassName('input2');
+    input.value = value + 1;
+}
+
+
+document.getElementsByClassName
